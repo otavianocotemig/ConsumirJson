d2f85cb2cba634f5e84289550603a7fa0d58d80b
otavianocotemig <otaviano@cotemig.com.br>
2020-10-09 10:00:42 -0300

    Adicionar arquivos de projeto.

---
+++ ConsumirJson/Default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ConsumirJson.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="false">
+                <Columns>
+                    <asp:BoundField DataField="userId" HeaderText="userId" />
+                    <asp:BoundField DataField="id" HeaderText="id" />
+                    <asp:BoundField DataField="title" HeaderText="title" />
+                    <asp:BoundField DataField="body" HeaderText="body" />
+                </Columns>
+            </asp:GridView>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ ConsumirJson/Default.aspx.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ConsumirJson
+{
+    public partial class Default : System.Web.UI.Page
+    {
+              
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GridView1.DataSource = GetPost();
+            GridView1.DataBind();
+        }
+
+        public DataTable GetPost()
+        {
+            var requisicaoWeb = WebRequest.CreateHttp("http://jsonplaceholder.typicode.com/posts/");
+            requisicaoWeb.Method = "GET";
+            requisicaoWeb.UserAgent = "ReqOtaviano";
+            var resposta = requisicaoWeb.GetResponse();
+            var streamDados = resposta.GetResponseStream();
+            StreamReader reader = new StreamReader(streamDados);
+            string objResponse = reader.ReadToEnd();
+            var table = JsonConvert.DeserializeObject<DataTable>(objResponse);
+            return table;
+
+
+
+        }
+
+        
+    }
+}
